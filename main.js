13ae3167536d5ef7fc9f97747e30ac494845e435
Miclane1 <125152607+Miclane1@users.noreply.github.com>
2023-04-11 19:36:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+noseY=0;
+noseX=0;
+
+function preload()
+{
+    my_filter=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    
+    canvas.center();     
+
+   video=createCapture(VIDEO);
+
+   video.size(300, 300);
+
+   video.hide();
+
+   poseNet=ml5.poseNet(video, modelLoaded);
+
+   poseNet.on('pose', gotPoses);
+
+
+   
+}
+
+    
+
+
+    function modelLoaded()
+    {
+        console.log("PoseNet Is initialized");
+    }
+
+    function gotPoses(results)
+    {
+        if(results.length>0)
+        {
+            console.log(results);
+
+            noseX=results[0].pose.nose.x-19;
+            noseY=results[0].pose.nose.y-10;
+
+            console.log(" NOSEX- " +noseX + " NOSEY- "+noseY);
+        }
+    }
+
+
+function draw()
+{
+      
+    image(video, 0, 0, 300, 300);
+
+    image(my_filter, noseX, noseY, 40, 40);
+
+    
+   
+
+
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
